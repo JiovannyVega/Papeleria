b4a2fb88d0d352b570dc500bd19354d4efd304ca
JiovannyVega <erickjiovannygamezvega@hotmail.com>
2023-11-24 14:33:39 -0600

    Primer commit

---
+++ src/papeleria/Papeleria.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package papeleria;
+
+import Login.Inicio;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Papeleria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Inicio I=new Inicio();
+     I.setVisible(true);
+    }
+    
+}
